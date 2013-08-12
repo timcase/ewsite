0bdf82a3cc0bfb09b014e603f6223ff35118ff0e
Tim Case <tim@2drops.net>
2013-08-12 11:56:34 -0700

    add welcomes resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Epsite::Application.routes.draw do
+  resources :welcomes
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcomes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -39,7 +41,7 @@ Epsite::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
---
+++ spec/controllers/welcomes_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe WelcomesController do
+
+end
