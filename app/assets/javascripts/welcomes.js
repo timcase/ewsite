626fc6b83ec1d348f8a455b67ba474ac0542447d
Tim Case <tim@2drops.net>
2013-08-25 12:36:32 -0700

    move code to welcomes js

---
+++ app/assets/javascripts/welcomes.js
@@ -0,0 +1,24 @@
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-43474866-1']);
+  _gaq.push(['_trackPageview']);
+
+$(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
+    $('#actions a').bind('click',function(event){
+        var $anchor = $(this);
+
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500,'easeInOutExpo');
+        /*
+        if you don't want to use the easing effects:
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000);
+        */
+        event.preventDefault();
+    });
+});
