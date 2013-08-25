cd08bc897778167b6230c04ccd3df60a6e3a96de
Tim Case <tim@2drops.net>
2013-08-25 12:22:46 -0700

    remove echowrap config

--- config/initializers/echowrap.rb
+++
@@ -1,5 +0,0 @@
-Echowrap.configure do |config|
-  config.api_key = 'FIM8YZ2YSRNEFFP00'
-  config.consumer_key = '8eb469289622a468af6f249a71bd9752'
-  config.shared_secret = 'XiLujtkqRnm2dh5MRYPvig'
-end
