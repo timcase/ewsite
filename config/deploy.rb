7b7867e0840dbe6cd1b825789c0e8723947fe491
Tim Case <tim@2drops.net>
2013-08-25 12:23:20 -0700

    change deploy for ewsite

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 require 'bundler/capistrano'
 load 'deploy/assets'
 
-set :application, "epsite"
-set :repository,  "git@github.com:timcase/epsite.git"
+set :application, "ewsite"
+set :repository,  "git@github.com:timcase/ewsite.git"
 set :port, 2170
 set :environment, :production
 set :scm, :git
 set :branch, 'master'
 
-set :deploy_to, '/var/www/epsite'
+set :deploy_to, '/var/www/ewsite'
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -26,7 +26,10 @@ ssh_options[:forward_agent] = true
 ssh_options[:port] = port
 
 # if you want to clean up old releases on each deploy uncomment this:
- after "deploy:restart", "deploy:cleanup"
+after "deploy:restart", "deploy:cleanup"
+after "deploy:update_code", "deploy:migrate"
+after "deploy:migrate", "deploy:cp_secret_token"
+after "deploy:cp_secre_token", "deploy:cp_echowrap"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -38,4 +41,14 @@ ssh_options[:port] = port
    task :restart, :roles => :app, :except => { :no_release => true } do
      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
    end
+
+
+   task :cp_secret_token do
+     run "cd #{release_path}/config/initializers; cp /var/www/ewsite/shared/secret_token.rb secret_token.rb;"
+   end
+
+   task :cp_echowrap do
+     run "cd #{release_path}/config/initializers; cp /var/www/ewsite/shared/echowrap.rb echowrap.rb;"
+   end
+
  end
