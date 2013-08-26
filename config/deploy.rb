aba3d30af2abf34cd955b993c3f6f073db7f59d9
Tim Case <tim@2drops.net>
2013-08-25 17:32:57 -0700

    fix deploy events

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ ssh_options[:port] = port
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
-after "deploy:migrate", "deploy:cp_secret_token"
+after "deploy:cleanup", "deploy:cp_secret_token"
 after "deploy:cp_secre_token", "deploy:cp_echowrap"
 
 # if you're still using the script/reaper helper you will need
